80cc2e9a585c93f0c569c61eb74a1044de84f98c
corey-scheinfeld <cscheinfeld@middlebury.edu>
2022-02-15 23:01:21 -0500

    wordGrid

--- src/wordGrid.js
+++ src/wordGrid.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState, useEffect, useRef } from "react";
-import wordGrid from './wordGrid.js';
-
 
 function App() {
   const [word, setWord] = useState("Apple");
   return (
     <div className="App">
       <h1>Cordle</h1>
-      <wordGrid/>
     </div>
   );
 }
