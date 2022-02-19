287bb2aac63452fcc55d90ead13775c81791b272
corey-scheinfeld <cscheinfeld@middlebury.edu>
2022-02-18 23:34:23 -0500

    word additions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import wordList from "./wordList.json";
 import ShareModal from "./ShareModal";
 
 function App() {
-  const [word, setWord] = useState("catan");
+  const [word, setWord] = useState("skier");
   const [guess, setGuess] = useState([]);
   const [err, setErr] = useState(false);
   const [win, setWin] = useState(false);
