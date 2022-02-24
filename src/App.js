a32d28b027efabdbfc786cd7fb6bda393d2d884b
corey-scheinfeld <cscheinfeld@middlebury.edu>
2022-02-23 22:11:57 -0500

    new word

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import wordList from "./wordList.json";
 import ShareModal from "./ShareModal";
 
 function App() {
-  const [word, setWord] = useState("clash");
+  const [word, setWord] = useState("drums");
   const [guess, setGuess] = useState([]);
   const [err, setErr] = useState(false);
   const [win, setWin] = useState(false);
